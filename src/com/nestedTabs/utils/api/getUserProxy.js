b7dda68fb60d662c7931b580033eef92ded67dc3
Mohsen Biglari <mohsen.biglari.1990@gmail.com>
2020-07-17 19:47:18 +0430

    reorder userProxy Fn based on completed

--- src/com/nestedTabs/utils/api/getUserProxy.js
+++ src/com/nestedTabs/utils/api/getUserProxy.js
@@ -2,11 +2,10 @@ const getUserProxy = function (ctx) {
     const proxy = {
         getSelectedTabsHistory: function () { return ctx.getSelectedTabsHistory(); },
         getMutableCurrentOptions: function () { return ctx.getMutableCurrentOptions(); },
-        resetToInitailOption: function () { ctx.resetToInitailOption(); return this; },
         getData: function () { return ctx.getData(); },
+        getTabObj: function (tabId) { return ctx.getTabObj(tabId); },
         isActiveTab: function (tabId) { return ctx.isActiveTab(tabId); },
         isOpenTab: function (tabId) { return ctx.isOpenTab(tabId); },
-        openAllTab: function () { return ctx.openAllTab(); },
         switchTab: function (tabId) { return ctx.switchTab({ id: tabId, e: null }); },
         deactiveTab: function () { return ctx.deactiveTab(null); },
         switchToNxtSiblingTab: function () { return ctx.switchToNxtSiblingTab({ e: null }); },
@@ -17,7 +16,9 @@ const getUserProxy = function (ctx) {
         closeTab: function (tabId, switchBeforeClose) { return ctx.closeTab({ id: tabId, e: null, switchBeforeClose }); },
         forceUpdate: function () { return ctx.forceUpdate(); },
         clearPanelsCache: function (panelId) { ctx.clearPanelsCache(panelId); return this; },
-        getTabObj: function (tabId) { return ctx.getTabObj(tabId); }
+
+        resetToInitailOption: function () { ctx.resetToInitailOption(); return this; },
+        openAllTab: function () { return ctx.openAllTab(); }
     };
     return proxy;
 };
