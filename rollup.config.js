737274f5d3f1f1b5462c477f4a4fbc567f3c6733
dev-javascript <javascript.code.dev@gmail.com>
2021-04-04 16:49:17 +0430

    add babel config, rollup config and webpack config + update package.json

---
+++ rollup.config.js
@@ -0,0 +1,24 @@
+import { terser } from 'rollup-plugin-terser';
+import commonjs from '@rollup/plugin-commonjs';
+import nodeResolve from '@rollup/plugin-node-resolve';
+const requirePolyfills = process.env.INCLUDE_POLYFILLS;
+export default {
+    input: requirePolyfills ? 'lib/esm-including-polyfills/index.js' : 'lib/esm/index.js',
+    output: {
+        file: requirePolyfills ? 'dist/react-dyn-tabs.including-polyfills.umd.min.js' : 'dist/react-dyn-tabs.umd.min.js',
+        format: 'umd',
+        name: 'useDynTabs',
+        globals: {
+            react: 'React',
+        },
+        sourcemap: true,
+    },
+    plugins: [
+        terser(),
+        commonjs(),
+        nodeResolve({ preferBuiltins: false }), // or `true`
+    ],
+    external: function (id) {
+        return (/react$|react-dom$|.test.js$|.js.snap$|.css$/g).test(id);
+    }
+};
