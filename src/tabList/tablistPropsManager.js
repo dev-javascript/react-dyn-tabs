582f43b1c1edb7459aadb494c64829af76a352aa
dev-javascript <javascript.code.dev@gmail.com>
2021-04-10 01:07:40 +0430

    fix missing direction class problem

--- src/tabList/tablistPropsManager.js
+++ src/tabList/tablistPropsManager.js
@@ -2,28 +2,10 @@ export default function ({ api }) {
     const _setting = api.optionsManager.setting
         , _op = api.optionsManager.options
         , result = {
-            className: _setting.tablistClass + ' ' + _setting[_op.direction + 'class']
+            className: _setting.tablistClass + ' ' + _setting[_op.direction + 'Class']
         };
     if (_op.accessibility) {
         result.role = 'tablist';
     }
     return result;
 };
-// Object.create({
-//     get: function ({ api }) {
-//         this._setting = api.optionsManager. ;
-//         this._op = api.optionsManager.options;
-//         return this._getA11Y(this._getBase());
-//     },
-//     _getBase: function () {
-//         return {
-//             className: this._setting.tablistClass + ' ' +
-//                 this._setting[this._op.direction + 'class']
-//         };
-//     },
-//     _getA11Y: function (obj) {
-//         if (this._op.accessibility)
-//             obj.role = 'tablist';
-//         return obj;
-//     }
-// });
