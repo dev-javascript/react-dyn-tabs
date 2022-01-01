125024c10c53b0a275758a366c86e7bca9e5b5d3
dev-javascript <javascript.code.dev@gmail.com>
2022-01-02 01:54:28 +0330

    change external field

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,8 @@
 import {terser} from 'rollup-plugin-terser';
-import commonjs from '@rollup/plugin-commonjs';
-import nodeResolve from '@rollup/plugin-node-resolve';
+import pkg from './package.json';
 const requirePolyfills = process.env.INCLUDE_POLYFILLS;
 export default {
-  input: requirePolyfills ? 'lib/esm-including-polyfills/index.js' : 'lib/esm/index.js',
+  input: requirePolyfills ? 'lib/esm-including-polyfills/index.js' : pkg.module,
   output: {
     file: requirePolyfills ? 'dist/react-dyn-tabs.including-polyfills.umd.min.js' : 'dist/react-dyn-tabs.umd.min.js',
     format: 'umd',
@@ -15,8 +14,9 @@ export default {
     },
     sourcemap: true,
   },
-  plugins: [terser(), commonjs(), nodeResolve({preferBuiltins: false})],
-  external: function (id) {
-    return /prop-types$|react$|react-dom$|.test.js$|.js.snap$|.css$/g.test(id);
-  },
+  plugins: [terser()],
+  external: [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.peerDependencies || {})],
+  // external: function (id) {
+  //   return /prop-types$|react$|react-dom$|.test.js$|.js.snap$|.css$/g.test(id);
+  // },
 };
