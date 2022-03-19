bede01efc49c57c58cad3fc9030d536e5fb3bc79
dev-javascript <javascript.code.dev@gmail.com>
2022-03-19 16:33:56 +0330

    update src/index.js : export more-button

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
 import useDynamicTabs from './useDynamicTabs/index.js';
+import moreButton from './plugins/more-button/virtualizor/index.js';
 export default useDynamicTabs;
+export const MoreButton = moreButton;
