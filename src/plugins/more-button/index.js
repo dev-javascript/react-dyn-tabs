85f1ef76e74c2a7313fb83897f5f34023d1aca1f
dev-javascript <javascript.code.dev@gmail.com>
2022-03-26 20:04:48 +0430

    del more-button/virtualizor  and move basic/index.js into more-button folder

--- src/plugins/more-button/index.js
+++ src/plugins/more-button/index.js
@@ -1,3 +1,3 @@
-import resizeDetectorIns from '../../../adapters/resize-detector-adapter/index.js';
+import resizeDetectorIns from '../../adapters/resize-detector-adapter/index.js';
 import Basic from './basic.js';
 export default Basic.bind(null, resizeDetectorIns);
--- src/plugins/more-button/virtualizor/index.js
+++
@@ -1,5 +0,0 @@
-import {VirtualizorConstructor, VirtualizorMethods} from './virtualizor.js';
-import resizeDetectorIns from '../../../adapters/resize-detector-adapter/index.js';
-import Main from '../basic/basic.js';
-Object.assign(VirtualizorConstructor.prototype, Main.prototype, VirtualizorMethods);
-export default VirtualizorConstructor.bind(null, {resizeDetectorIns, Main});
