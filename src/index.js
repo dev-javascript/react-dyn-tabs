2ceb3ee9cac32b440daf6d38ec21ff69ecace760
dev-javascript <javascript.code.dev@gmail.com>
2022-03-26 20:05:08 +0430

    update src/index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import useDynamicTabs from './useDynamicTabs/index.js';
-import moreButton from './plugins/more-button/basic/index.js';
+import moreButton from './plugins/more-button/index.js';
 export default useDynamicTabs;
 export const MoreButton = moreButton;
