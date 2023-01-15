c7b2432b78146872c679124daa9f41465699aca2
dev-javascript <javascript.code.dev@gmail.com>
2023-01-16 01:59:57 +0330

    update tablistPorpsManager : sliderEl should get rtlClass and verticalClass

--- src/tabList/tablistPropsManager.js
+++ src/tabList/tablistPropsManager.js
@@ -3,16 +3,18 @@ export default function ({optionsManager: {setting: _setting, options: _op}}) {
     result = {
       tablistRootProps: {className: tablistRootClass},
       tablistSectionProps: {className: tablistSectionClass},
-      tablistSliderProps: {className: tablistSliderClass},
+      tablistSliderProps: {className: tablistSliderClass + ' ' + tablistClass},
       tablistProps: {className: tablistClass},
     };
   if (_op.isVertical) {
     result.tablistRootProps.className += ' ' + _setting.verticalClass;
     result.tablistProps.className += ' ' + _setting.verticalClass;
+    result.tablistSliderProps.className += ' ' + _setting.verticalClass;
   }
   if (_op.direction === 'rtl') {
     result.tablistRootProps.className += ' ' + _setting.rtlClass;
     result.tablistProps.className += ' ' + _setting.rtlClass;
+    result.tablistSliderProps.className += ' ' + _setting.rtlClass;
   }
   if (_op.accessibility) {
     result.tablistProps.role = 'tablist';
