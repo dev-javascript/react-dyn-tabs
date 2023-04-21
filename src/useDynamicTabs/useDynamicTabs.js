b1e92f51cdf7898adff46310f4de45d1f557c8a8
HarishBaskaran-CA <harish.baskaran@go-yubi.com>
2023-04-21 16:15:56 +0530

    nextjs build issues

--- src/useDynamicTabs/useDynamicTabs.js
+++ src/useDynamicTabs/useDynamicTabs.js
@@ -1,8 +1,8 @@
-import React, {useState, useReducer, useLayoutEffect, useRef} from 'react';
+import React, {useState, useReducer, useEffect, useRef} from 'react';
 function useDynamicTabs(getDeps, options = {}) {
   const {reducer, getApiInstance, PanelList, TabList, ApiContext, StateContext, ForceUpdateContext} = getDeps();
-  const ref = useRef(null);
-  if (ref.current === null)
+  const ref = useRef("null");
+  if (ref.current === "null")
     ref.current = {api: getApiInstance(options), TabListComponent: null, PanelListComponent: null};
   const {
       current: {api},
@@ -11,28 +11,28 @@ function useDynamicTabs(getDeps, options = {}) {
     [state, dispatch] = useReducer(reducer, api.optionsManager.initialState),
     [flushState, setFlushState] = useState({});
   api.updateStateRef(state, dispatch).updateFlushState(setFlushState);
-  useLayoutEffect(() => {
+  useEffect(() => {
     api.updateState(state);
   }, [state]);
-  useLayoutEffect(() => {
+  useEffect(() => {
     api.trigger('onLoad', api.userProxy);
     return () => {
       api.trigger('onDestroy', api.userProxy);
     };
   }, []);
-  useLayoutEffect(() => {
+  useEffect(() => {
     api.trigger('onInit', api.userProxy);
   });
-  useLayoutEffect(() => {
+  useEffect(() => {
     api.trigger('_onReady', api.userProxy);
   }, []);
-  useLayoutEffect(() => {
+  useEffect(() => {
     const oldState = api.previousState,
       [openedTabIDs, closedTabIDs] = api.helper.getArraysDiff(state.openTabIDs, oldState.openTabIDs),
       isSwitched = oldState.selectedTabID !== state.selectedTabID;
     api.onChange({newState: state, oldState, closedTabIDs, openedTabIDs, isSwitched});
   }, [state]);
-  useLayoutEffect(() => {
+  useEffect(() => {
     api.trigger('_onFlushEffects', api.userProxy, () => {
       return [{currentData: api.getData(), instance: api.userProxy}];
     });
