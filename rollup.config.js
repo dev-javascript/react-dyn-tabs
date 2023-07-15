9a62ed15c04bd5d8ecbc01e5658579e65a05a529
dev-javascript <javascript.code.dev@gmail.com>
2023-07-16 02:42:22 +0330

    fix general style

--- rollup.config.js
+++ rollup.config.js
@@ -2,38 +2,38 @@ import {terser} from 'rollup-plugin-terser';
 import commonjs from '@rollup/plugin-commonjs';
 import nodeResolve from '@rollup/plugin-node-resolve';
 const Config = ({en, inputPath = '', outputFile = 'rc-dyn-tabs-core', outputName = 'useDynTabs', pf = false}) => {
-  var pfName = pf ? '.including-polyfills' : '';
-  return {
-    input: `lib/${pf ? 'esm-including-polyfills' : 'esm'}/${inputPath}index.js`,
-    output: {
-      file: `dist/${outputFile}${pfName}.umd${en === 'dev' ? '' : '.min'}.js`,
-      format: 'umd',
-      name: outputName,
-      globals: {
-        'prop-types': 'PropTypes',
-        'react-dom': 'ReactDOM',
-        react: 'React',
+    var pfName = pf ? '.including-polyfills' : '';
+    return {
+      input: `lib/${pf ? 'esm-including-polyfills' : 'esm'}/${inputPath}index.js`,
+      output: {
+        file: `dist/${outputFile}${pfName}.umd${en === 'dev' ? '' : '.min'}.js`,
+        format: 'umd',
+        name: outputName,
+        globals: {
+          'prop-types': 'PropTypes',
+          'react-dom': 'ReactDOM',
+          react: 'React',
+        },
+        sourcemap: true,
       },
-      sourcemap: true,
-    },
-    plugins: (function () {
-      const _plugins = [nodeResolve({preferBuiltins: false}), commonjs()];
-      if (en === 'prod') {
-        _plugins.push(terser());
-      }
-      return _plugins;
-    })(),
-    external: function (id) {
-      return /prop-types$|react$|react-dom$|.test.js$|.js.snap$|.css$/g.test(id);
-    },
-  };
-};
-const ConfigFactory = (op) => [
-  Config({en: 'dev', ...op}),
-  Config({en: 'prod', ...op}),
-  Config({en: 'dev', pf: true, ...op}),
-  Config({en: 'prod', pf: true, ...op}),
-];
+      plugins: (function () {
+        const _plugins = [nodeResolve({preferBuiltins: false}), commonjs()];
+        if (en === 'prod') {
+          _plugins.push(terser());
+        }
+        return _plugins;
+      })(),
+      external: function (id) {
+        return /prop-types$|react$|react-dom$|.test.js$|.js.snap$|.css$/g.test(id);
+      },
+    };
+  },
+  ConfigFactory = (op) => [
+    Config({en: 'dev', ...op}),
+    Config({en: 'prod', ...op}),
+    Config({en: 'dev', pf: true, ...op}),
+    Config({en: 'prod', pf: true, ...op}),
+  ];
 export default ConfigFactory().concat(
   ConfigFactory({
     outputFile: 'rc-dyn-tabs-responsive-plugin',
--- src/tablistContainer/tablistContainer.js
+++ src/tablistContainer/tablistContainer.js
@@ -1,14 +1,6 @@
 import React from 'react';
 import {ApiContext} from '../utils/context.js';
 export default function TablistContainer(props) {
-  const {
-    options: {isVertical},
-    setting: {tablistContainerClass, verticalClass},
-  } = React.useContext(ApiContext).optionsManager;
-
-  let _className = tablistContainerClass;
-  if (isVertical) {
-    _className += ' ' + verticalClass;
-  }
+  const _className = React.useContext(ApiContext).optionsManager.setting.tablistContainerClass;
   return <div className={_className}>{props.children}</div>;
 }
