c33ce7163c29d2698b9ff964b60093d63f50a81c
dev-javascript <javascript.code.dev@gmail.com>
2023-07-24 02:25:32 +0330

    export MoreButtonPlugin from src/index.js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
 import useDynamicTabs from './useDynamicTabs/index.js';
+import moreButtonPlugin from './plugins/moreButtonPlugin/index.js';
 export default useDynamicTabs;
+export const MoreButtonPlugin = moreButtonPlugin;
