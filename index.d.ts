811cbbdd7598625c4c15f2d53372c646953d7cda
dev-javascript <javascript.code.dev@gmail.com>
2023-11-28 23:54:48 +0330

    fix types

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,4 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, FC, PropsWithChildren } from 'react';
 export type Callback = (instance: Instance) => void;
 /**
  * - ready function accepts a callback as its parameter and executes it as soon as Tabs get mounted.
@@ -26,30 +26,36 @@ export interface TabComponentProps {
     api: Instance;
     tabProps: TabProps;
     iconProps?: IconProps;
+    children: React.ReactNode;
+}
+export interface PanelProps {
+    id: string;
+    isSelected: boolean;
+    api: Instance;
 }
 export interface Options {
     /** * default value is "ltr"*/
     direction?: 'rtl' | 'ltr';
-    defaultPanelComponent?: () => FunctionComponent<{ id: string, isSelected: boolean, api: Instance }> | null;
-    tabComponent?: FunctionComponent<TabComponentProps>;
+    defaultPanelComponent?: React.ReactElement<any, any> | FunctionComponent<PanelProps> | React.ComponentClass<PanelProps>;
+    tabComponent?: React.ComponentClass<TabComponentProps> | ((props: TabComponentProps) => JSX.Element);
     selectedTabID?: string;
     tabs?: Array<TabData>;
     /**     * default value is true     */
     accessibility?: boolean;
     /**     * default value is false     */
     isVertical?: boolean;
-    onLoad?: () => void;
-    onInit?: () => void;
-    onChange?: ({ currentData, previousData, closedTabIDs, openedTabIDs }: { currentData: any, previousData: any, closedTabIDs: Array<string>, openedTabIDs: Array<string> }) => void;
+    onLoad?(this: Instance): void;
+    onInit?(this: Instance): void;
+    onChange?(this: Instance, { currentData, previousData, closedTabIDs, openedTabIDs }: { currentData: CurrentData, previousData: CurrentData, closedTabIDs: Array<string>, openedTabIDs: Array<string> }): void;
     /**     * defautl value function returns true     */
-    beforeSelect?: (e: React.MouseEvent<HTMLElement>, id: string) => boolean;
-    onFirstSelect?: ({ currentSelectedTabId, previousSelectedTabId }: { currentSelectedTabId: string, previousSelectedTabId: string }) => void;
-    onSelect?: ({ currentSelectedTabId, previousSelectedTabId }: { currentSelectedTabId: string, previousSelectedTabId: string }) => void;
-    onOpen?: (openedTabIDs: Array<string>) => void;
+    beforeSelect?(this: Instance, e: React.MouseEvent<HTMLElement>, id: string): boolean;
+    onFirstSelect?(this: Instance, { currentSelectedTabId, previousSelectedTabId }: { currentSelectedTabId: string, previousSelectedTabId: string }): void;
+    onSelect?(this: Instance, { currentSelectedTabId, previousSelectedTabId }: { currentSelectedTabId: string, previousSelectedTabId: string }): void;
+    onOpen?(this: Instance, openedTabIDs: Array<string>): void;
     /**     * defautl value function returns true     */
-    beforeClose?: (e: React.MouseEvent<HTMLElement>, id: string) => boolean;
-    onClose?: (closedTabIDs: Array<string>) => void;
-    onDestroy?: () => void;
+    beforeClose?(this: Instance, e: React.MouseEvent<HTMLElement>, id: string): boolean;
+    onClose?(this: Instance, closedTabIDs: Array<string>): void;
+    onDestroy?(): void;
 }
 export interface TabData {
     id?: string;
@@ -62,37 +68,35 @@ export interface TabData {
     iconClass?: string;
     /**     * default value is false     */
     disable?: boolean;
-    panelComponent?: React.ReactElement<any, any> | FunctionComponent<{ id: string, isSelected: boolean, api: Instance }> | null;
-    [x: string]: unknown;
+    panelComponent?: React.ReactElement<any, any> | FunctionComponent<PanelProps> | React.ComponentClass<PanelProps> | null;
+    [x: string]: any;
 }
 export interface CurrentData {
     openTabIDs: Array<string>;
     selectedTabID: string;
 }
 export interface Instance {
-    isOpen: (tabID: string) => boolean;
-    open: (tabData: TabData) => Promise<{ currentData: CurrentData, instance: Instance }>;
-    isSelected: (tabID: string) => boolean;
-    select: (tabID: string) => Promise<{ currentData: CurrentData, instance: Instance }>;
+    isOpen(tabID: string): boolean;
+    open(tabData: TabData): Promise<{ currentData: CurrentData, instance: Instance }>;
+    isSelected(tabID: string): boolean;
+    select(tabID: string): Promise<{ currentData: CurrentData, instance: Instance }>;
     /**
      * - When switching parameter is true, it switches to previous selected tab
      */
-    close: (tabID: string, switching?: boolean) => Promise<{ currentData: CurrentData, instance: Instance }>;
-    refresh: () => Promise<{ currentData: CurrentData, instance: Instance }>;
-    getOption: (optionName: string) => any;
-    setOption: (optionName: string, optionValue: any) => Instance;
-    getTab: (tabID: string) => TabData;
-    setTab: (tabID: string, sourceObject: TabData) => Instance;
-    on: (eventName: string, handler: Function) => Instance;
-    one: (eventName: string, handler: Function) => Instance;
-    off: (eventName: string, handler: Function) => Instance;
-    getData: () => TabData;
-    getPreviousData: () => TabData;
-    sort: (tabIDs: Array<string>) => Promise<{ currentData: CurrentData, instance: Instance }>;
+    close(tabID: string, switching?: boolean): Promise<{ currentData: CurrentData, instance: Instance }>;
+    refresh(): Promise<{ currentData: CurrentData, instance: Instance }>;
+    getOption(optionName: string): any;
+    setOption(optionName: string, optionValue: any): Instance;
+    getTab(tabID: string): TabData;
+    setTab(tabID: string, sourceObject: TabData): Instance;
+    on(eventName: string, handler: Function): Instance;
+    one(eventName: string, handler: Function): Instance;
+    off(eventName: string, handler: Function): Instance;
+    getData(): CurrentData;
+    getPreviousData(): CurrentData;
+    sort(tabIDs: Array<string>): Promise<{ currentData: CurrentData, instance: Instance }>;
 }
-type Tablist = FunctionComponent<{}>;
+type Tablist = FC<PropsWithChildren<{}>>;
 type Panellist = FunctionComponent<{}>;
-export const TabList: Tablist;
-export const PanelList: Panellist;
 declare const useDynTabs: (options?: Options) => [Tablist, Panellist, Ready];
 export default useDynTabs;
