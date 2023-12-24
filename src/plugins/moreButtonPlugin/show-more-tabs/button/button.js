1905f2089cc2d83c2cd424bc96002161a861de41
dev-javascript <javascript.code.dev@gmail.com>
2023-12-24 19:37:03 +0330

    change show more button element from span to div tag

--- src/plugins/moreButtonPlugin/show-more-tabs/button/button.js
+++ src/plugins/moreButtonPlugin/show-more-tabs/button/button.js
@@ -30,14 +30,14 @@ export default function Button(getDeps, props) {
   );
   return (
     <>
-      <span onClick={onClick} ref={btnRef} className={props.buttonClassName}>
+      <div onClick={onClick} ref={btnRef} className={props.buttonClassName}>
         <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512">
           <path
             fill="gray"
             d="M470.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L402.7 256 265.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160zm-352 160l160-160c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L210.7 256 73.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0z"
           />
         </svg>
-      </span>
+      </div>
       {open ? <Popper {...props} ref={popperRef} btnRef={btnRef} className={props.popperClassName} /> : null}
     </>
   );
