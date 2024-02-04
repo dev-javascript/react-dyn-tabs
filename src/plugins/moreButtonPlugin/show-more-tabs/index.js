ac39f48e8ff17a4d346495b7542539bd3864f3bd
dev-javascript <javascript.code.dev@gmail.com>
2024-02-04 15:19:15 +0330

    move button folder from moreButtonPlugin/show-more-tabs into moreButtonPlugin folder

--- src/plugins/moreButtonPlugin/show-more-tabs/index.js
+++ src/plugins/moreButtonPlugin/show-more-tabs/index.js
@@ -1,7 +1,7 @@
-import React, {memo} from 'react';
+import React, { memo } from 'react';
 import ElManagement from '../elementManagement/index.js';
 import Api from './api.js';
-import Button from './button/index.js';
+import Button from '../button/index.js';
 import resizeDetectorIns from './element-resize-detector-adapter.js';
 import ShowMoreTabs from './show-more-tabs.js';
 const getDeps = () => {
